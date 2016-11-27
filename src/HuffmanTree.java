86ac30f81c310e17c0e929ce8fa19f8409ceab35
Chiara <zizzachi@grinnell.edu>
2016-11-26 19:00:18 -0600

    Update HuffmanTree.java

--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -55,17 +55,18 @@ public class HuffmanTree {
 		@Override
 		public int compare(Node n1, Node n2) {
 			if (n1.frequency > n2.frequency) {
-				return 1; // was 1
+				return 1;
 			} else if (n1.frequency < n2.frequency) {
-				return -1; // was -1
+				return -1;
 			} else {
-				return 0; // was 0
+				return 0;
 			}
 		}
 	}
 
 	private Node root;
 	private String[] arr;
+	private Map<String, Integer> map;
 
 	/** Constructs a HuffmanTree from the given frequency map of 9-bit values
 	 * @param m a Map.
@@ -88,7 +89,7 @@ public class HuffmanTree {
 	private PriorityQueue<Node> buildQueue(Map<Short, Integer> m) {
 		Comparator<Node> compareNode = new NodeComparator();
 		PriorityQueue<Node> pq = new PriorityQueue<Node>(compareNode);
-
+		
 		/* Insert EOF into Map */
 		m.put((short) 256, 1);
 
@@ -130,39 +131,63 @@ public class HuffmanTree {
 	 * @param out the BitOutputStream being written to
 	 */
 	public void encode(BitInputStream in, BitOutputStream out) {
-		System.out.println("In encode.");
-		int c = 0;
-		c = in.readBits(8);
+		//System.out.println("In encode.");
+		boolean proceed = true;
+		int c = in.readBits(8);
 
-		while (c != -1) {
-			System.out.println("In while loop of encode.");
-			String temp = this.arr[c];
+		while (proceed) {
+			/* Checks to see if the EOF has been reached */
+			if(c == -1) {
+				proceed = false;
+				c = 256; //array index for EOF character
+			}
+			
+			String temp = this.arr[c];  //temp is the "01" string designated for each character
+			//System.out.println(temp);
 			for (int i = 0; i < temp.length(); i++) {
+				//System.out.println(temp.charAt(i));
 				if (temp.charAt(i) == '0') {
-					System.out.println("In the '0' part.");
 					out.writeBit(0);
 				} else if (temp.charAt(i) == '1') {
-					System.out.println("In the '1' part.");
 					out.writeBit(1);
 				} else {
 					throw new IllegalArgumentException("Not '0' or '1'");
 				}
-
 			}
+			//System.out.println();
 			c = in.readBits(8);
 		}
 	}
-
+	
 	/** Decodes a stream of huffman codes from a file given as a stream of bits
-	 * into their uncompressed form, saving the results to the given output
-	 * stream
+	 * into their uncompressed form, saving the results to the given output stream
 	 * @param in the BitInputStream being read
 	 * @param out the BitOutputStream being written to
 	 */
 	public void decode(BitInputStream in, BitOutputStream out) {
-
+		String temp = "";
+		boolean proceed = true;
+		while(proceed) {
+			if(in.readBit() == -1) { //unnecessary?
+				proceed = false;
+			}
+			temp += in.readBit();
+			System.out.println(temp);
+			if(this.map.containsKey(temp)) {
+				out.writeBits(this.map.get(temp), 3);
+				System.out.println(temp);
+				System.out.println(this.map.get(temp));
+				System.out.println();
+				temp = "";
+			}
+		}
 	}
 
+	
+	
+	
+	
+	/* EXTRA CODES */
 	public void buildPath(Node cur, String s) {
 		if (cur.left != null && cur.right != null) {
 			buildPath(cur.left, s + "0");
@@ -170,22 +195,41 @@ public class HuffmanTree {
 		} else {
 			this.arr[cur.character] = s;
 		}
+		
+		createDecodeMap(); //from this new array, construct a corresponding map to decode it
+	}
+	
+	private void createDecodeMap() {
+		this.map = new HashMap<String, Integer>();
+		for(int i = 0; i < this.arr.length; i ++) {
+			if(this.arr[i] != null) {
+				this.map.put(this.arr[i], i);
+			}
+		}
 	}
 
+	
+	
+	
+	
 	public void printArr() {
-		for (int i = 0; i < this.arr.length; i++) { // CHECK THIS -- DOESN'T
-			// MATCH HOMEWORK CHART
+		for (int i = 0; i < this.arr.length; i++) { // CHECK THIS -- DOESN'T MATCH HOMEWORK CHART
 			if (this.arr[i] != null) {
 				System.out.println("Character value: " + i);
 				System.out.println("Character path:  " + this.arr[i] + "\n");
 			}
 		}
 	}
+	
+
+	
+	
+	
 
 	public static void main(String[] args) throws IOException {
 		Map<Short, Integer> m = new HashMap<Short, Integer>();
 
-		// ask about frequencies
+		// ask about frequencies -- using array mapping from lab
 		m.put((short) 'z', 1);
 		m.put((short) 'a', 3);
 		m.put((short) 'b', 2);
@@ -194,21 +238,46 @@ public class HuffmanTree {
 		HuffmanTree t = new HuffmanTree(m);
 
 		t.buildPath(t.root, "");
-		t.printArr(); // added
+		t.printArr();
 
-		System.out.println("Tree frequency: " + t.root.frequency);
+		//System.out.println("Tree frequency: " + t.root.frequency);
 
-		BitInputStream input = new BitInputStream("TestInput.txt");
-		BitOutputStream output = new BitOutputStream("TestOutput.txt", true);
+		BitInputStream inputEncode = new BitInputStream("TestInput.txt");
+		BitOutputStream outputEncode = new BitOutputStream("TestOutput.txt", true);
+		
+		
+		
 
-		System.out.println(input.readBits(8));
-		t.encode(input, output);
+		/* Convert from characters to 01s */
+		t.encode(inputEncode, outputEncode);
+		
+		inputEncode.close();
+		outputEncode.close();
 		// output.writeBit(1);
+		
+		
+		
+		
+		BitInputStream inputDecode = new BitInputStream("TestOutput.txt");
+		BitOutputStream outputDecode = new BitOutputStream("TestDecode.txt", true);
+		
+		/* Convert from 01s to characters */
+		t.decode(inputDecode, outputDecode);
+		
+		
+		/* MAP CREATION TEST */
+//		for(int i = 0; i < t.map.keySet().toArray().length; i++) {
+//			System.out.println(t.map.keySet().toArray()[i]);
+//			System.out.println(Character.toChars(t.map.get(t.map.keySet().toArray()[i])));
+//			System.out.println();
+//		}
 
-		System.out.println(input.hasBits());
 
-		input.close();
-		output.close();
+		//System.out.println(inputEncode.hasBits());      /////////////SEEMS TO BE BROKEN?
+		//System.out.println(inputDecode.hasBits());
+
+		inputDecode.close();
+		outputDecode.close();
 
 	}
 }
